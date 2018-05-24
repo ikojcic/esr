7fb9b0ca180e1a9ca1ca9060c88440892209d33a
Samuel Deslauriers-Gauthier <sam.deslauriers@gmail.com>
2018-05-24 17:26:54 +0200

    FEAT: Add setup.py and esr package
    This commit adds a setup.py file to be able to install the esr with
    pip.

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='esr',
+    version='0.0.0',
+    packages=['esr'],
+    url='https://gitlab.com/sdeslauriers/esr.git',
+    license='GNU GPLv3',
+    author='Foundation project',
+    author_email='',
+    description='A Python package to evaluate EEG and MEG source '
+                'reconstructions using simulations.'
+)
